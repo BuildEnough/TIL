0d89f408ef7d8538a2aa4abc06df0c68404cb583
BuildEnough <rhtmaehcl878@naver.com>
2023-10-14 22:14:56 +0900

    백준 1546 평균

---
+++ Java/src/Algorithm/_1546_평균_Scanner.java
@@ -0,0 +1,21 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class _1546_평균_Scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long sum = 0;
+        long max = 0;
+
+        for(int i = 0; i < N; i++) {
+            int temp = sc.nextInt();
+            if(temp > max) {
+                max = temp;
+            }
+            sum += temp;
+        }
+        System.out.println(sum * 100.0 / max / N);
+    }
+}
---
+++ Java/src/Algorithm/_1546_평균_Scanner_practice.java
@@ -0,0 +1,8 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class _1546_평균_Scanner_practice {
+    public static void main(String[] args) {
+    }
+}
