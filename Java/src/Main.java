93820f98e5e8a32a5ad99313fcd9df7f43600ab4
BuildEnough <rhtmaehcl878@naver.com>
2023-10-15 20:10:00 +0900

    백준 11659 구간 합 구하기

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "cat";
+        String result = "";
+        for(int i=0; i<str.length(); i++) {
+            result += str.charAt(i);
+            for(int j=0; j<i+1; j++) {
+                result += str.charAt(j);
+            }
+        }
+        System.out.println(result);
     }
 }
