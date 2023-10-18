b6c77020928e84ebf6c38e1c8ddd40a335ed4e10
BuildEnough <rhtmaehcl878@naver.com>
2023-10-18 22:25:34 +0900

    백준 1546 평균 sudo 코드

---
+++ Java/src/Algorithm/_1546_평균_Scanner_2.java
@@ -0,0 +1,23 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class _1546_평균_Scanner_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        long sum = 0;
+        long max = 0;
+        for (int i = 0; i < N; i++) {
+            if(A[i]>max) max = A[i];
+            sum += A[i];
+        }
+        System.out.println(sum * 100 / max / N);
+    }
+}
--- Java/src/Algorithm/_1546_평균_Scanner_practice.java
+++ Java/src/Algorithm/_1546_평균_Scanner_practice.java
@@ -1,8 +1,7 @@
 package Algorithm;
 
-import java.util.Scanner;
-
 public class _1546_평균_Scanner_practice {
     public static void main(String[] args) {
+
     }
 }
