e78a1e5a0eb7f1c758b2fe97228355d9fa4d821c
BuildEnough <rhtmaehcl878@naver.com>
2023-11-02 11:37:33 +0900

    옵션 처리

--- Spring/core/src/main/java/hello/core/AutoAppConfig.java
+++ Spring/core/src/main/java/hello/core/AutoAppConfig.java
@@ -16,8 +16,10 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+/*
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+*/
 }
