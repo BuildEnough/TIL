5258f822c442f0b511dbbc5d212628814096a385
BuildEnough <rhtmaehcl878@naver.com>
2023-12-22 13:04:39 +0900

    springboot 연습

--- Spring/springboot-developer1/src/main/java/org/example/springbootdeveloper/service/BlogService.java
+++ Spring/springboot-developer1/src/main/java/org/example/springbootdeveloper/service/BlogService.java
@@ -3,10 +3,13 @@ package org.example.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import org.example.springbootdeveloper.domain.Article;
 import org.example.springbootdeveloper.dto.AddArticleRequest;
+import org.example.springbootdeveloper.dto.UpdateArticleRequest;
 import org.example.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.plaf.PanelUI;
+import java.util.List;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
@@ -17,5 +20,32 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 전체 조회 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    // 블로그 글 조회 메서드
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    // 블로그 글 삭제 메서드
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    // 블로그 글 수정 메서드
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
