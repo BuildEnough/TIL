1425c7f0c126057c79c64bb8388e036031fb4d26
BuildEnough <rhtmaehcl878@naver.com>
2024-01-27 22:22:23 +0900

    Test

--- Java/backend-java-basic/src/Main.java
+++ Java/backend-java-basic/src/Main.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List list = new ArrayList<String>();
+
+        list.add("public");
+        list.add("static");
+        list.add("void");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        list.stream().forEach(str -> System.out.println(str));
     }
 }
---
+++ Java/eclipse/thisisjava/src/ch01/sec09/Hello.java
@@ -0,0 +1,23 @@
+package ch01.sec09;
+
+
+/**
+ * 
+ * @author enough
+ */
+
+/*
+ 자바 시작 
+ 2024.1.27
+ */
+public class Hello {
+
+
+	// 프로그램 실행 진입
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문 
+		System.out.println("Hello world");
+
+	}
+
+}
