31e33b931adde6d75d00e4cefd0e8205507188be
BuildEnough <rhtmaehcl878@naver.com>
2024-02-02 19:38:29 +0900

    Java 공부 연습

---
+++ Java/eclipse/thisisjava/src/ch06/sec13/exam03/package1/B.java
@@ -0,0 +1,15 @@
+package ch06.sec13.exam03.package1;
+
+public class B {
+	public void method() {
+		A a = new A();
+		
+		a.field1 = 2;
+		a.field2 = 2;
+//		a.field3 = 2;
+		
+		a.method1();
+		a.method2();
+//		a.method3();
+	}
+}
---
+++ Java/eclipse/thisisjava/src/ch07/sec04/exam02/SupersonicAirplaneExample.java
@@ -0,0 +1,23 @@
+package ch07.sec04.exam02;
+
+public class SupersonicAirplaneExample {
+
+	public static void main(String[] args) {
+		SupersonicAirplane sa = new SupersonicAirplane();
+		sa.takeOff();
+		sa.fly();
+		System.out.println();
+		
+		sa.flyMode = SupersonicAirplane.SUPERSONIC;
+		sa.fly();
+		System.out.println();
+		
+		sa.flyMode = SupersonicAirplane.NORMAL;
+		sa.fly();
+		sa.land();
+		
+		
+
+	}
+
+}
