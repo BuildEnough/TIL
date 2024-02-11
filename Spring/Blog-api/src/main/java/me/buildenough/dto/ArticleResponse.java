677d0f4345b37a3139a4e643f8c5cca30f4e27c9
BuildEnough <rhtmaehcl878@naver.com>
2024-02-11 13:25:42 +0900

    Blog-api: 글 목록 조회 API 구현

---
+++ Spring/Blog-api/src/main/java/me/buildenough/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package me.buildenough.dto;
+
+import lombok.Getter;
+import me.buildenough.domain.Article;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
