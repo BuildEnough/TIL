c61f6c675490afc95cd3961618bb72abbbf2aa6d
BuildEnough <rhtmaehcl878@naver.com>
2024-02-11 12:07:38 +0900

    Blog-api: 서비스 메서드 생성

---
+++ Spring/Blog-api/src/main/java/me/buildenough/dto/AddArticleRequest.java
@@ -0,0 +1,21 @@
+package me.buildenough.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.buildenough.domain.Article;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+    private String title;
+    private String content;
+
+    public Article toEntity() {
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
