5d1c7ec757f760204c7a2abddb7e41d79003cf5f
BuildEnough <rhtmaehcl878@naver.com>
2024-02-11 19:40:59 +0900

    Blog-api: SpringSecurity 서비스 메서드 코드 작성

---
+++ Spring/Blog-api/src/main/java/me/buildenough/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package me.buildenough.service;
+
+import lombok.RequiredArgsConstructor;
+import me.buildenough.domain.User;
+import me.buildenough.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
