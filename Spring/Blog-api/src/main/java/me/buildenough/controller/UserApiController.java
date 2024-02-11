29b4cbb425bee0d190a08c127a0aa733512db898
BuildEnough <rhtmaehcl878@naver.com>
2024-02-11 19:56:19 +0900

    Blog-api: 회원가입, 로그인 뷰 작성(html)

--- Spring/Blog-api/src/main/java/me/buildenough/controller/UserApiController.java
+++ Spring/Blog-api/src/main/java/me/buildenough/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.buildenough.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.buildenough.dto.AddUserRequest;
 import me.buildenough.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -16,6 +21,12 @@ public class UserApiController {
     public String signup(AddUserRequest request) {
         userService.save(request); // 회원가입 메서드 호출
         return "redirect:/login"; // 회원가입 후 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 
 }
