953d037fdb3029b7dff01b78cb8df5f662be1a71
BuildEnough <rhtmaehcl878@naver.com>
2024-02-16 22:06:48 +0900

    자바 연습 파일 생성

---
+++ Java/IntelliJ/ch1/sec06/Hello.java
@@ -0,0 +1,9 @@
+package ch1.sec06;
+
+public class Hello {
+    // 프로그램 실행 진입점
+    public static void main(String[] args) {
+        // 콘솔에 출력하는 실행문
+        System.out.println("Hello Java");
+    }
+}
