eac65cfc160a98ac8092aed995c3289595754417
BuildEnough <rhtmaehcl878@naver.com>
2024-02-22 23:51:40 +0900

    자바 연습

---
+++ Java/study1/src/com/javalec/calex/CalculationEx.java
@@ -0,0 +1,33 @@
+package com.javalec.calex;
+
+public class CalculationEx {
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 3;
+		int h = 5;
+		
+//		System.out.println("산술 연산자");
+//		System.out.println(i+j); // 13
+//		System.out.println(i-j); // 7
+//		System.out.println(i*j); // 30
+//		System.out.println(i/j); // 3
+//		System.out.println(i%j); // 1
+		
+//		System.out.println("자동증감 연산자");
+//		i++;
+//		System.out.println(i); // 10 -> 11
+//		j--;
+//		System.out.println(j); // 3 -> 2
+		
+//		System.out.println("동등비교 연산자");
+//		System.out.println(i == j); // false
+//		System.out.println(i != j); // true
+//		System.out.println(i > j); // true
+//		System.out.println(i < j); // false
+		
+		System.out.println("논리 연산자");
+		System.out.println();
+		
+	}
+
+}
---
+++ Java/study1/src/com/javalec/ex/TestEx.java
@@ -0,0 +1,18 @@
+package com.javalec.ex;
+
+public class TestEx {
+	public static void main(String[] args) {
+		
+		int i = 10;
+		double d = i;
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println();
+		
+		double d2 = 3.14D;
+		int i2 = (int) d2;
+		System.out.println(d2);
+		System.out.println(i2);
+	}
+
+}
