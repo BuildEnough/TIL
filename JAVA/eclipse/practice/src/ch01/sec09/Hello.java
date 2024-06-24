d6de70fd9db51301508157e36cba936f506896eb
BuildEnough <rhtmaehcl878@naver.com>
2024-06-24 10:48:48 +0900

    1장 완료

---
+++ JAVA/eclipse/practice/src/ch01/sec09/Hello.java
@@ -0,0 +1,18 @@
+package ch01.sec09;
+
+
+/**
+ * 
+ * @author enough
+ */
+public class Hello {
+
+
+	// 프로그램 실행 진입
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문 
+		System.out.println("Hello world");
+
+	}
+
+}
