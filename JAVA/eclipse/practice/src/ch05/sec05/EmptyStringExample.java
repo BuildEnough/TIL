e8a9b8ca47bbfce6dc3b5e31f9275966c4eebb78
BuildEnough <rhtmaehcl878@naver.com>
2024-06-27 19:49:19 +0900

    자바 예제 정리

---
+++ JAVA/eclipse/practice/src/ch05/sec05/EmptyStringExample.java
@@ -0,0 +1,13 @@
+package ch05.sec05;
+
+public class EmptyStringExample {
+
+	public static void main(String[] args) {
+		String empty = "";
+		if(empty.equals("")) {
+			System.out.println("empty 변수가 참조하는 String 객체는 빈 문자열");
+		}
+
+	}
+
+}
