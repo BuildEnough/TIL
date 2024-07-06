c0dd8def0eafb38b916fcf48793214b1c83cae17
BuildEnough <rhtmaehcl878@naver.com>
2024-07-06 19:02:55 +0900

    폴더 정리

---
+++ JavaScript/javascript/02/js/sample.js
@@ -0,0 +1,4 @@
+document.write('welcome <br>')
+document.write('<h1>welcome</h1> <br>')
+document.write(2+3)
+alert('환영')
---
+++ 코딩테스트/practice.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class practice {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    private static int[] solution(int[] arr) {
+        Integer[] result = Arrays.stream(arr).boxed().distinct().toArray()
+    }
+}
