1455db9775e0878b15da7df7aafda6f41b3a4f1e
BuildEnough <rhtmaehcl878@naver.com>
2024-08-25 20:12:30 +0900

    TIL 정리

---
+++ C#/sol1/Hello/Hello/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        // program start
+        static void Main(String[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법: Hello.exe");
+                return;
+            }
+            WriteLine("Hello, {0}", args[0]);
+        }
+    }
+}
