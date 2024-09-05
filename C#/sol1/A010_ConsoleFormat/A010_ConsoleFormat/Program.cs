09e6e574d844abb8d99adb328fc6e7395bf1761f
BuildEnough <rhtmaehcl878@naver.com>
2024-09-05 23:38:52 +0900

    C# 공부

---
+++ C#/sol1/A010_ConsoleFormat/A010_ConsoleFormat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A010_ConsoleFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Standard Numeric Format Specifiers");
+            Console.WriteLine(
+                "(C) Currency: . . . . . ."
+                );
+        }
+    }
+}
---
+++ C#/sol1/A012_FloatDoubleDecimal/A012_FloatDoubleDecimal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A012_FloatDoubleDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flt = 1F / 3;
+            double dbl = 1D / 3;
+            decimal dcm = 1M / 3;
+
+            Console.WriteLine("float: {0}\ndouble: {1}\ndecimal: {2}", flt, dbl, dcm);
+            Consoel
+        }
+    }
+}
