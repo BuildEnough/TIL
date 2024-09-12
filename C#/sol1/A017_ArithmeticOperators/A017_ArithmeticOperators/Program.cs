7a5d01c4a675326a1e2cd79882a56bf127fa22a6
BuildEnough <rhtmaehcl878@naver.com>
2024-09-12 20:59:48 +0900

    C# 정리

---
+++ C#/sol1/A017_ArithmeticOperators/A017_ArithmeticOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace A017_ArithmeticOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("정수의 계산");
+            Console.WriteLine(123 + 45);
+            Console.WriteLine(123 - 45);
+            Console.WriteLine(123 * 45);
+            Console.WriteLine(123 / 45);
+            Console.WriteLine(123 % 45);
+
+            Console.WriteLine("실수의 계산");
+            Console.WriteLine(123.45 + 67.89);
+            Console.WriteLine(123.45 - 67.89);
+            Console.WriteLine(123.45 * 67.89);
+            Console.WriteLine(123.45 / 67.89);
+            Console.WriteLine(123.45 % 67.89);
+
+        }
+    }
+}
