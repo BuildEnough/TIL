e954a110ffe7f61351de32e3a98e8be88cf98d0c
BuildEnough <rhtmaehcl878@naver.com>
2024-09-22 13:58:50 +0900

    C# 실습

---
+++ C#/sol1/A019_Overflow/A019_Overflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A019_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int.MaxValue = {0}", int.MaxValue);
+            int x = int.MaxValue, y = 0;
+
+            checked
+            {
+                try
+                {
+                    y = x + 10;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.Write("int.MaxValue + 10 = {0}", y);
+        }
+    }
+}
